59ddc435afe7fe9b5fbc7680311e7c418e31b64d
yasirghouri <yasir.ag7@gmail.com>
2021-01-22 01:06:55 +0500

    added Job component and its functionality

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Github Jobs App
 
+## Import React Bootstrap CDN
+
+go to https://react-bootstrap.github.io/getting-started/introduction/ and copy paste the link of CSS CDN into your index.html file in /public
+
 ## Package Dependencies
 
 1. npm i axios
 2. npm i react-bootstrap
+3. npm i react-markdown
--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import React, { useState } from "react";
 import "./App.css";
 import useFetchJobs from "./hooks/useFetchJobs";
 import { Container } from "react-bootstrap";
+import Job from "./components/Job/Job";
 
 function App() {
-  const { jobs, loading, error } = useFetchJobs();
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error } = useFetchJobs(params, page);
   return (
-    <Container>
+    <Container className="my-4">
+      <h1 className="mb-4">Github Jobs</h1>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try Refreshing</h1>}
-      <h1>{jobs?.length}</h1>
+      {jobs.map((job) => {
+        return <Job key={job.id} job={job} />;
+      })}
     </Container>
   );
 }
